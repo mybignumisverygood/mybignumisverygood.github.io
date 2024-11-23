482ed185254d791837cb69b5438d1d4aeadb9d64
mybignumisverygood <lion_ghr@163.com>
2024-11-23 14:05:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ function handle(){
 }
 */
 
+var msg = [];
 var ordmsg = 0;
-var your_name;
+var your_name, what1 = false;
 
 function judgement(){
 	myth_pwd = document.getElementById("pwd").value;
@@ -22,64 +23,76 @@ function judgement(){
 	if(myth_pwd == "3yAG7x=="){
 		next_phase();
 	} else { //一堆奇奇怪怪的对话
-		var new_msg = document.createElement("h2");
-		var what1 = false;
-		new_msg.setAttribute("id", "msg_" + ordmsg);
-		new_msg.setAttribute("class", "showing_text_effect");
 		const my_names = ["搞到", "高导", "高鸿睿", "狗睿", "被Lost我", "我的世界彡犭乄丶", "Lg1t6_", "lg123456_", "G-Lion"];
-		const meaningless = ["!", "！", ".", "。", "/", " ", ";", "啊", "呀", "呢", "哈"];
+		const meaningless = ["!", "！", ".", "。", "/", " ", ";", "啊", "呀", "呢", "哈", "吖", "哦"];
 		for(let i = myth_pwd.length - 1; i >= 0; i--){
-			if (!meaningless.includes(myth_pwd[i])) {break;}
+			if (!meaningless.includes(myth_pwd[i])) break;
 			else myth_pwd = myth_pwd.slice(0, -1);
 		}
 		if (!myth_pwd){
-			new_msg.innerHTML = "你说了什么吗……?";
+			createNewMsg("你说了什么吗……?");
 		} else if (myth_pwd == "我"){
+			createNewMsg("你……! " + (what1 ? "又" : "") + "怎么了吗?");
 			what1 = true;
-			new_msg.innerHTML = "你……!" + (what1 ? "又" : "") + "怎么了吗?";
 		} else if (myth_pwd == "你"){
+			createNewMsg("我……! " + (what1 ? "又" : "") + "怎么了吗?");
 			what1 = true;
-			new_msg.innerHTML = "我……!" + (what1 ? "又" : "") + "怎么了吗?";
 		} else if (my_names.includes(myth_pwd)){
-			new_msg.innerHTML = myth_pwd + "，在此!/.";
+			createNewMsg(myth_pwd + "，在此!/.");
 		} else if (myth_pwd == "你好"){
-			new_msg.innerHTML = "你好！";
+			createNewMsg("你好！");
 		} else if (myth_pwd == "再见"){
-			new_msg.innerHTML = "再见……!";
+			createNewMsg("再见……!");
 			setInterval("window.open('', '_self').close();", 2500);
-		} else if ((myth_pwd.substring(0,2) == "我是" || myth_pwd.substring(0,2) == "我叫") && myth_pwd.slice(2).substring(0,1) != "谁"){
+		} else if ((myth_pwd.substring(0, 2) == "我是" || myth_pwd.substring(0, 2) == "我叫") && myth_pwd.slice(2).substring(0,1) != "谁"){
+			// 自我介绍
 			let k1 = false;
-			if (your_name){
-				let new_name_temp= document.createElement("h2");
-				new_name_temp.setAttribute("class", "showing_text_effect");
-				if (your_name == myth_pwd.slice(2)) {new_name_temp.innerHTML = "我知道呢, 你不是说过吗?"; k1 = true;}
-				else new_name_temp.innerHTML = "哇你改名了吗? 总之我会记住这个最新的名字的——";
-				document.body.appendChild(new_name_temp);
+			if (your_name){ // 你已经有名字了啊……
+				createNewMsg();
+				if (your_name == myth_pwd.slice(2)) {createNewMsg("我知道呢, 你不是说过吗?"); k1 = true;}
+				else createNewMsg("哇你改名了吗? 总之我会记住这个最新的名字的——");
 			}
-			your_name = myth_pwd.slice(2); //你的名字!
-			if (my_names.includes(your_name)) new_msg.innerHTML = "哇你的名字竟然和我一样诶! 我记住了呢! 你现在可以问问我你是谁/";
-			else if (your_name == "") new_msg.innerHTML = "嗯……你是什么……?";
-			else if (!k1) new_msg.innerHTML = "你好, " + your_name+ "! 我记住了呢, 你现在可以问问我你是谁/";
+			your_name = myth_pwd.slice(2); // 你的名字!
+			if (plantheases(your_name)){
+				createNewMsg();
+				createNewMsg(plantheases(your_name));
+			}	
+			if (my_names.includes(your_name)) createNewMsg("哇你的名字竟然和我一样诶! 我记住了呢! 你现在可以问问我你是谁/");
+			else if (your_name == "") createNewMsg("嗯……你是什么……?");
+			else if (!k1) createNewMsg("你好, " + your_name+ "! 我记住了呢, 你现在可以问问我你是谁/");
 		} else if (myth_pwd == "我是谁"){
-			if (your_name) new_msg.innerHTML = "你是" + your_name + "///";
-			else new_msg.innerHTML = "嗯……我不知道啊, 不过你可以告诉我";
+			if (your_name) createNewMsg("你是" + your_name + "///");
+			else createNewMsg("嗯……我不知道啊, 不过你可以告诉我");
 		} else if (myth_pwd == "你是谁"){
-			new_msg.innerHTML = "你可以叫我" + my_names.join("或");
+			createNewMsg("你可以叫我" + my_names.join("或"));
 		} else if (["普朗西斯语", "普朗西斯", "Plantheas", "plantheas"].includes(myth_pwd)){
-			new_msg.innerHTML = "Yearing! 原来你也<u style = 'background: linear-gradient(to right, red, blue); -webkit-background-clip: text; -webkit-text-fill-color: transparent;'>Plantheas</u>!";
-		} else if (["涨到", "张导", "张轩宁", "zhang萱琳"].includes(myth_pwd)){
-			new_msg.innerHTML = "哇你是涨到吗?";
-		} else if (["Wissea", "wissea", "吴亦萱", "物以轩"].includes(myth_pwd)){
-			new_msg.innerHTML = "这位是邪恶的人类😈!";
-		} else if (["Grace", "Harper", "圆圈小姐不是圆规小姐", "圆圈小姐就是圆规小姐", "或隐含", "霍尹涵", "虹屿怀", "好与坏"].includes(myth_pwd)){
-			new_msg.innerHTML = "你是圆规小姐吗?/";
+			createNewMsg("Yearing! 原来你也<u style = 'background: linear-gradient(to right, red, blue); -webkit-background-clip: text; -webkit-text-fill-color: transparent;'>Plantheas</u>!");
 		} else {
-			new_msg.innerHTML = "抱歉暂时还没有这么多对话呢/ 其实算上这个只有十个左右而已";
+			createNewMsg("抱歉我暂时还听不懂 '" + myth_pwd + "' 呢/ 其实算上这个只有十个对话左右而已");
 		}
-		document.body.appendChild(new_msg);
-		ordmsg++;
 	}
 	return "哇你是能看到这条消息的人类！";
+}
+
+function plantheases(x){
+	for(let i = 0; i <= 3; i++){
+		if(x.includes(["涨到", "张导", "张轩宁", "zhang萱琳"][i])) return "哇你是涨到吗?";
+	} 
+	for(let i = 0; i <= 3; i++){
+		if(x.includes(["Wissea", "wissea", "吴亦萱", "物以轩"][i])) return "这位是邪恶的人类😈!";
+	}
+	for(let i = 0; i <= 6; i++){
+		if(x.includes(["Harper", "圆圈小姐不是圆规小姐", "圆圈小姐就是圆规小姐", "或隐含", "霍尹涵", "虹屿怀", "好与坏"][i])) return "你是圆规小姐吗?/";
+	}
+}
+
+function createNewMsg(x){
+	msg[ordmsg] = document.createElement("h2");
+	msg[ordmsg].setAttribute("id", "msg_" + ordmsg);
+	msg[ordmsg].setAttribute("class", "showing_text_effect");
+	msg[ordmsg].innerHTML = x;
+	document.body.appendChild(msg[ordmsg]);
+	ordmsg++;
 }
 
 function next_phase(){
@@ -91,4 +104,3 @@ function next_phase(){
 	document.body.appendChild(d1);
 	return ;
 }
-
