3894b96d0cf305f27484ca1b6d4f522228c693fc
mybignumisverygood <lion_ghr@163.com>
2024-11-23 23:32:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-// Phase 1: 从一切的源头
-/*
-var sphase1 = document.getElementById('sphase1')
-var ph1 = document.getElementsByClassName("showing_text_effect");
-
-sphase1.addEventListener('animationstart', handle)
-function handle(){
-	for (i = 0; i < 6; i++){
-		ph1[0].style.cssText = ''
-		ph1[0].className = "sphase1";
-	}
-	sphase1.removeEventListener('animationstart', handle)
-}
-*/
-
 var msg = [];
 var ordmsg = 0;
-var your_name, what1 = false;
+var your_name;
+var what1 = what2 = false;
 
 function judgement(){
 	myth_pwd = document.getElementById("pwd").value;
@@ -23,9 +9,10 @@ function judgement(){
 	if(myth_pwd == "3yAG7x=="){
 		next_phase();
 	} else { //一堆奇奇怪怪的对话
-		const my_names = ["搞到", "高导", "高鸿睿", "狗睿", "被Lost我", "我的世界彡犭乄丶", "Lg1t6_", "lg123456_", "G-Lion"];
+		const my_names = ["搞到", "高导", "高鸿睿", "狗睿", "被Lost我", "我的世界彡犭乄丶", "Lg1t6_", "lg123456_", "G-Lion", "mcshanquanwuzhu", "mcsanquanwuzhu"];
 		const meaningless = ["!", "！", ".", "。", "/", " ", ";", "啊", "呀", "呢", "哈", "吖", "哦"];
-		for(let i = myth_pwd.length - 1; i >= 0; i--){
+		const regEng = /^[A-Za-z]+$/;
+		for (let i = myth_pwd.length - 1; i >= 0; i--){ // 剔除末尾无关的字符
 			if (!meaningless.includes(myth_pwd[i])) break;
 			else myth_pwd = myth_pwd.slice(0, -1);
 		}
@@ -44,6 +31,17 @@ function judgement(){
 		} else if (myth_pwd == "再见"){
 			createNewMsg("再见……!");
 			setInterval("window.open('', '_self').close();", 2500);
+		} else if (regEng.test(myth_pwd)){
+			createNewMsg("那个……全是英文字母的话我是看不懂的, 要不还是说中文吧");
+			what2 = true;
+		} else if (myth_pwd.includes("杀")){
+			// 主语是谁?
+			let subject = myth_pwd[myth_pwd.indexOf("杀") - 1];
+			if (subject == "我") createNewMsg("……?补药杀啊啊");
+			else if (subject == "你") createNewMsg("……这种事情不要拿我当主语啊啊");
+			else createNewMsg("不是发生什么了.?");
+		} else if (["普朗西斯语", "普朗西斯", "Plantheas", "plantheas"].includes(myth_pwd)){
+			createNewMsg("Yearing! 原来你也<u style = 'background: linear-gradient(to right, red, blue); -webkit-background-clip: text; -webkit-text-fill-color: transparent;'>Plantheas</u>!");
 		} else if ((myth_pwd.substring(0, 2) == "我是" || myth_pwd.substring(0, 2) == "我叫") && myth_pwd.slice(2).substring(0,1) != "谁"){
 			// 自我介绍
 			let k1 = false;
@@ -65,10 +63,9 @@ function judgement(){
 			else createNewMsg("嗯……我不知道啊, 不过你可以告诉我");
 		} else if (myth_pwd == "你是谁"){
 			createNewMsg("你可以叫我" + my_names.join("或"));
-		} else if (["普朗西斯语", "普朗西斯", "Plantheas", "plantheas"].includes(myth_pwd)){
-			createNewMsg("Yearing! 原来你也<u style = 'background: linear-gradient(to right, red, blue); -webkit-background-clip: text; -webkit-text-fill-color: transparent;'>Plantheas</u>!");
-		} else {
-			createNewMsg("抱歉我暂时还听不懂 '" + myth_pwd + "' 呢/ 其实算上这个只有十个对话左右而已");
+		} else { // 你在说什么我听不懂
+			if (!what2) createNewMsg("抱歉我暂时还听不懂 '" + myth_pwd + "' 呢/ 其实算上这个只有十个对话左右而已");
+			else {createNewMsg("抱歉虽然你这次不是全英文但我还是看不懂你在说什么……"); what2 = false;}
 		}
 	}
 	return "哇你是能看到这条消息的人类！";
