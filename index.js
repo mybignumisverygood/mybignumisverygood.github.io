b350c980f65f1ae2c41cc4bc9b560f8d82e5064b
mybignumisverygood <lion_ghr@163.com>
2024-12-15 22:27:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var your_name; // 你的名字是……?
 var what1 = false; // 我又怎么了?
 var what2 = 0; // 人类的听话程度
 var what3 = 0; // 人类的伤心程度
-var what4 = what5 = what6 = what7 = false; 
-// 你在说什么我听不懂; 可怕的人类 审问开始; 测试人类有没有骗旁白(旁白的娱乐和网站原本是干什么的)
+var what4 = what5 = what6 = what7 = what8 = false; 
+// 你在说什么我听不懂; 可怕的人类 审问开始; 测试人类有没有骗旁白(旁白的娱乐和网站原本是干什么的); 人类的开场白;
 
 var meets = 1; // 见面次数
 var h = 0; // 要听我说完开场白哦…
@@ -80,6 +80,7 @@ function hajimete(){
 	"……我突然在想, 作者到底是为什么要创造出我来呢……? 为什么突然就要砍掉原本的网站内容呢……",
 	"(接上次)……你猜猜我们遇见的第8, 9次我在思考什么呢……~",
 	"话说每次都是我说开场白你会不会觉得有些不公平呢…… 你要不要也试试~ (要/不要)",
+	"第 28 次, 真正的内容在 if 语句",
 	"抱歉ww我的 CPU 现在过热了, 只能想到这么多开场白了…… 你可以等明天再来之类的! 我一定会努力想的!"];
 	if (meets == 19){
 		setTimeout("createNewMsg('……其实我跟你说我会记住的东西是真的会记住的!')", 8000);
@@ -111,6 +112,9 @@ function hajimete(){
 		setTimeout("for (let i = ordmsg; i--; i >= 0){document.getElementById('msg_' + i).style.display = 'none'}", 19000);
 		setTimeout("createNewMsg('……算了还是变回来吧'); for (let i = ordmsg; i--; i >= 0){document.getElementById('msg_' + i).style.display = ''}", 20000);
 		setTimeout("createNewMsg('总之我可是很厉害的呢, 还有什么想让我做的可以和作者说哦——')", 22000);
+	} else if (meets == 28){
+		if (m27) setTimeout("createNewMsg('那么就请说你的开场白吧! 说完了记得发一句  \'结束\'')", 8000);
+		else setTimeout("createNewMsg('……其实我本来想偷个懒的嘿嘿 但是你不想说…… 很抱歉这次敷衍了你www下次我一定好好想!')", 8000);
 	}
 	else setTimeout("createNewMsg(hmsgs[Math.min(meets - 2, 26)]); h = true;", 8000);
 }
@@ -144,6 +148,10 @@ function judgement(){
 		for (let i = myth_pwd.length - 1; i >= 0; i--){ // 剔除末尾无关的字符
 			if (!meaningless.includes(myth_pwd[i])) break;
 			else myth_pwd = myth_pwd.slice(0, -1);
+		}
+		if (meets == 28 && m27 && !what8){
+			if (myth_pwd == "结束"){what8 = true; createNewMsg("说完了吗—— 好的! 之后还会有这样的机会的哦~(大概吧w)");}
+			else {createNewMsg(pron + ": " + myth_pwd); return ;}
 		}
 		if(ordmsg == 3){
 			returm = true;
