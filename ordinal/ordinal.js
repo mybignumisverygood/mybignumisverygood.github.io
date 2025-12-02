567f311d47a90812e1b74de917c9e1f7b0bcea22
mybignumisverygood <lion_ghr@163.com>
2025-12-02 19:39:10 +0800

    1

--- ordinal/ordinal.js
+++ ordinal/ordinal.js
@@ -22,17 +22,17 @@ function fund(){
 	fundEl.style.left = "calc(50% - "+fundEl.getBoundingClientRect().width/2+"px)";
 	fundEl.style.bottom = "calc(50% - "+fundEl.getBoundingClientRect().height/2+"px)";
 }
-function exit(){
+function exit(){ // 真是屈才您了呢
 	fundEl.style.display = "none";
 }
 
 function display(){
-	if(ordivar.lt(EN(42337))){
+	if(ordivar.lt(EN(42337))){ // <ε_ω
 		ordinum.innerHTML = formatWhole(number(ordivar));
 		ordinal.innerHTML = formaty(number(ordivar));
 	}else{
 		var p1 = formaty1(ordivar);
-		ordinum.innerHTML = p1[0];
+		ordinum.innerHTML = formatWhole(p1[0]);
 		ordinal.innerHTML = p1[1];
 	}
 	ordivar = ordivar.plus(1);
@@ -67,14 +67,29 @@ function formaty(x){
 		var expo = x.logBase(ep2).floor();
 		return "ε<sub>2</sub><sup>"+(NZ(expo.sub(1))?formaty(expo):"")+"</sup>";
 	} else {
-		return "ε<sub>"+formaty(number((ordivar.sub(42337).pow(1.1).add(1350))))+"</sub>";
+		return "ε<sub>ω</sub>";
 	}
 }
 // Beyond ε_ω
+// 现在开始就是数字随序数动了! 虽然…… 好像…… 有点麻烦
+
+const magic = EN('3.04365'); // 经过计算的神秘数字, 估算ε_x用
+function Epsilons(base){ // 什么叫数值逼近之力啊(骄傲)
+	return magic.tetr(base.mul(2).add(3));
+}
+
+const zeta = Epsilons(Epsilons(epsilon)); //~FF1.525e13
+
 function formaty1(x){
-	var base = number((x.sub(42337).pow(1.1).add(1350)));
-	var sbase = EN('3.04365').tetr(base.mul(2).add(3)); // 经过计算的神秘数字
-	return [formatWhole(sbase), "ε<sub>"+formaty(number((x.sub(42337).pow(1.1).add(1350))))+"</sub>"];
+	if(x.lt(EN(59364))){ // <ζ_0
+		if(x.lt(EN(42337))){
+			return [number(x), formaty(number(x))];
+		}
+		var base_x = x.sub(42337).pow(1.1).add(1350);
+		return [Epsilons(formaty1(base_x)[0]), "ε<sub>"+formaty1(base_x)[1]+"</sub>"];
+	} else {
+		return [zeta, "ζ<sub>0</sub>=ε<sub>ε<sub>ε<sub>0</sub></sub></sub>"];
+	}
 }
 	
 	
